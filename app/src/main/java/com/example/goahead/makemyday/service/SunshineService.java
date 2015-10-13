de93a9a62f66c28924130ed1a73186ddde45cd1c
afthabvp <afthab01@gmail.com>
2015-10-13 12:22:59 +0530

    Alarm reciever

--- app/src/main/java/com/example/goahead/makemyday/service/SunshineService.java
+++ app/src/main/java/com/example/goahead/makemyday/service/SunshineService.java
@@ -1,6 +1,7 @@
 package com.example.goahead.makemyday.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -283,5 +284,15 @@ public class SunshineService extends IntentService {
         return locationId;
     }
 
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(LOCATION_QUERY_EXTRA, intent.getStringExtra(LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+
+        }
+    }
 
 }
